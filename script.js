1cd787e3ec40344582d7cdeee1661218b396dcf8
AlexSSoares3 <alex.ssoares3@senacsp.edu.br>
2026-07-12 16:17:08 -0300

    Alterações feitas para melhorar o código

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-let idadeDoUsuario = 20;
 
-while (idadeDoUsuario < 0) {
-    console.log("Idade inválida. Digite novamente.");
+function validarCadastroUsuario(idadeDoUsuario) {
+    console.log(`--- Testando cenário com idade: ${idadeDoUsuario} ---`);
 
-    break;
-    
+   
+    let tentativas = 1;
+    while (tentativas > 0) {
+        tentativas--; 
+    }
+
+  
+    if (idadeDoUsuario < 0) {
+        console.error("Erro: A idade digitada não pode ser um número negativo. Por favor, revise.");
+    } else if (idadeDoUsuario === 0) {
+        console.error("Erro: A idade não pode ser zero. Digite um valor válido.");
+    } else if (idadeDoUsuario < 18) {
+        console.warn("Cadastro bloqueado: Sistema restrito para maiores de 18 anos.");
+    } else if (idadeDoUsuario >= 18 && idadeDoUsuario <= 120) {
+        console.log("Sucesso: Cadastro permitido! E-mail de confirmação enviado.");
+    } else {
+        console.error("Erro: Idade inserida está fora do limite aceitável pelo sistema.");
+    }
+    console.log("\n");
 }
 
-if (idadeDoUsuario >= 18) {
-    console.log("cadastro permitido");
-} else {
-    console.log("cadastro bloqueado: menor de 18 anos");
-}
+
+validarCadastroUsuario(20);  
+validarCadastroUsuario(15);  
+validarCadastroUsuario(-5);  
+validarCadastroUsuario(0);   
+validarCadastroUsuario(150); 
